3238a7bf1019a07bb52a535c57df5b7f67b8b127
CArmstrong2 <whitecrow119@gmail.com>
2023-01-08 20:24:50 -0800

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,21 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL UNSIGNED NOT NULL,
-    department_id INT UNSIGNED NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+)
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL
+    manager_id INT
 )
