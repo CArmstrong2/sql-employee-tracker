345f2311fbf121be535cf0e942c35df2da137c81
CArmstrong2 <whitecrow119@gmail.com>
2023-01-08 20:37:29 -0800

    added table relationships

--- db/schema.sql
+++ db/schema.sql
@@ -12,12 +12,18 @@ CREATE TABLE role (
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-)
+    INDEX department_index (department_id),
+    CONSTRAINT forkey_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL
-    manager_id INT
-)
+    role_id INT NOT NULL,
+    INDEX role_index (role_id),
+    CONSTRAINT forkey_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT,
+    INDEX manager_index (manager_id),
+    CONSTRAINT forkey_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
